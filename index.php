6efeeffc46f85319ad756a35849aaa61bc4569dc
fab <fabrizio.salmi@gmail.com>
2016-11-20 11:58:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,176 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="bootstrap theme, bootstrap template, html5 theme">
+    <meta name="description" content="Search the web for copyright infringement. Automated monitoring made by experts. DMCA takedown service.">
+    <link rel="shortcut icon" type="image/png" href="favicon.png"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+
+    <link rel="stylesheet" type="text/css" href="https://copyrightscan.com/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://copyrightscan.com/style.css">
+    <link rel="stylesheet" href="https://copyrightscan.com/css/animate.css"/>
+    <script src="https://copyrightscan.com/js/jquery-2.1.0.js"></script>
+    <script src="https://copyrightscan.com/js/bootstrap.js"></script>
+    <script src="https://copyrightscan.com/js/typer.js"></script>
+    <script src="https://copyrightscan.com/js/blocs.js"></script>
+    <link rel='stylesheet' href='https://copyrightscan.com/css/et-line.min.css'/>
+    <link rel='stylesheet' href='https://copyrightscan.com/css/font-awesome.min.css'/>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <title>Copyright Scan</title>
+    
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"https://copyrightscan.com/policy.html","theme":"dark-top"};
+</script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+<!-- End Cookie Consent plugin -->
+
+</head>
+<body>
+<!-- Main container -->
+<div class="page-container">
+
+    <!-- Navigation Bloc -->
+    <div class="bloc bgc-carmine-pink d-bloc" id="nav-bloc">
+        <div class="container">
+            <nav class="navbar row">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html"><span class="et-icon-gears icon-sm icon-white zoomIn animated"></span> Copyright Scan</a>
+                    <button id="nav-toggle" type="button" class="ui-navbar-toggle navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-1">
+                        <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
+                            class="icon-bar"></span><span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse navbar-1">
+                    <ul class="site-navigation nav navbar-nav">
+                        <li>
+                            <a  onclick="scrollToTarget('#services')">Services</a>
+                        </li>
+                        <li>
+                            <a onclick="scrollToTarget('#quote')">Contact us</a>
+                        </li>
+   
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navigation Bloc END -->
+
+    <!-- header -->
+    <div class="bloc bgc-outer-space bg-city-overlay d-bloc" id="header">
+        <div class="container bloc-xl">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3 class="text-center mg-lg tc-white">WE ARE SPECIALIST IN <span
+                            data-typer-targets="ONLINE MEDIA MONITORING FOR COPYRIGHT INFRINGEMENT"></span></h3>
+					                    <p class="text-center  mg-lg animated fadeInUp animDelay02">Copyright infringement is the use of works protected by copyright law without permission, infringing certain exclusive rights granted to the copyright holder, such as the right to reproduce, distribute, display or perform the protected work, or to make derivative works.</p>
+		
+                    <p class="text-center  mg-lg animated fadeInUp animDelay02">
+                        Every day a lot of content is uploaded on the web. Search, inspect and report stuff for copyright infringements is an impossible task unless you are a copyright scan expert. We also build and share automated scripts to speed up the job and let you concentrate on your business.
+                    </p>
+
+                    <div class="text-center" style="visibility: visible;font-size: large;color: beige;">
+                        <a class="btn  btn-xl btn-wire wire-btn-carmine-pink animDelay06 animated fadeInUp" href="mailto:fabrizio.salmi@gmail.com">CONTACT</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- header END -->
+
+    <!-- services -->
+    <div class="bloc l-bloc bgc-white" id="services">
+        <div class="container bloc-lg">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="text-center">
+                        <span class="et-icon-picture icon-xl icon-carmine-pink animated zoomIn animDelay02"></span>
+                    </div>
+                    <h3 class="text-center mg-md ">
+                        Easy to use
+                    </h3>
+
+                    <p class="text-center">
+                        All stuff in just one page.
+                    </p>
+                </div>
+                <div class="col-sm-4">
+                    <div class="text-center">
+                        <span class="et-icon-gears icon-xl icon-carmine-pink animated zoomIn"></span>
+                    </div>
+                    <h3 class="text-center mg-md ">
+                        Robust
+                    </h3>
+
+                    <p class="text-center">
+                        API service available.
+                    </p>
+                </div>
+                <div class="col-sm-4">
+                    <div class="text-center">
+                        <span class="et-icon-linegraph icon-xl icon-carmine-pink animated zoomIn animDelay02"></span>
+                    </div>
+                    <h3 class="text-center mg-md ">
+                        Powerful
+                    </h3>
+
+                    <p class="text-center">
+                        Multiple takedowns. 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- services END -->
+
+
+    
+    <!-- Footer - quote -->
+    <div class="bloc bgc-carmine-pink d-bloc" id="quote">
+        <div class="container bloc-md">
+            <div class="row">
+                <div class="col-sm-9">
+                    <h3 class="mg-md animated zoomIn">
+                        Don&rsquo;t hesitate to contact us now...
+                    </h3>
+                </div>
+                <div class="col-sm-3 text-center">
+                    <a href="/cdn-cgi/l/email-protection#680e090a1a01120107461b09040501280f05090104460b0705" class="btn btn-lg wire-btn-white btn-wire animated zoomIn">MAIL US</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer - quote END -->
+
+   
+
+    <!-- ScrollToTop Button -->
+    <a class="bloc-button btn btn-d scrollToTop" onclick="scrollToTarget('1')"><span
+            class="fa fa-chevron-up"></span></a>
+    <!-- ScrollToTop Button END-->
+
+    <!-- Footer - bloc-8 -->
+    <div class="bloc bgc-platinum l-bloc" id="bloc-8">
+        <div class="container bloc-sm">
+                   </div>
+    </div>
+    <div class="row" style="text-align:center;font-size:small;">
+                CopyrightScan.com © 2016
+            </div>
+    <!-- Footer - bloc-8 END -->
+</div>
+<!-- Main container END -->
+<script type="text/javascript">/* <![CDATA[ */(function(d,s,a,i,j,r,l,m,t){try{l=d.getElementsByTagName('a');t=d.createElement('textarea');for(i=0;l.length-i;i++){try{a=l[i].href;s=a.indexOf('/cdn-cgi/l/email-protection');m=a.length;if(a&&s>-1&&m>28){j=28+s;s='';if(j<m){r='0x'+a.substr(j,2)|0;for(j+=2;j<m&&a.charAt(j)!='X';j+=2)s+='%'+('0'+('0x'+a.substr(j,2)^r).toString(16)).slice(-2);j++;s=decodeURIComponent(s)+a.substr(j,m-j)}t.innerHTML=s.replace(/</g,'&lt;').replace(/>/g,'&gt;');l[i].href='mailto:'+t.value}}catch(e){}}}catch(e){}})(document);/* ]]> */</script></body>
+
+<!-- Google Analytics -->
+
+<!-- Google Analytics END -->
+
+</html>
+
